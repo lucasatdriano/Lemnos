21d87031bc853ed9392197a2dabcb2bb3a302e6d
L7643 <lucasatdriano@gmail.com>
2024-04-27 17:26:02 -0300

    add style in productPage

--- src/pages/productPage/Product.jsx
+++ src/pages/productPage/Product.jsx
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './product.scss';
+import img from '../../assets/logoHorizontal.svg'
+import iconAddCart from '../../assets/iconAddCart.svg';
+import { MdFavoriteBorder, MdFavorite  } from "react-icons/md";
 
 export function Product() {
     const { id } = useParams();
     const navigate = useNavigate();
     const BRL = new Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' });
     const [product, setProduct] = useState(null);
+    const [mainImage, setMainImage] = useState(null);
+    const [isFavorite, setIsFavorite] = useState(false);
 
     const productData = {
         name: "Apple 27\" iMac Desktop Computer (16GB RAM, 1TB HDD, Intel Core i5)",
@@ -22,10 +27,23 @@ export function Product() {
         weight: "5kg"
     };
 
+    useEffect(() => {
+        setMainImage("https://via.placeholder.com/150");
+    }, []);
+
+    const handleImageClick = (image) => {
+        setMainImage(image);
+    };
+
+    const handleAddToCart = () => {
+        console.log("Produto adicionado ao carrinho:", productData.name);
+    };
+
+    const handleAddToFavorites = () => {
+        setIsFavorite((prevIsFavorite) => !prevIsFavorite);
+    };
+
     // useEffect(() => {
-    //   // Aqui você faria uma solicitação para sua fonte de dados (API, banco de dados, etc.)
-    //   // para buscar os detalhes do produto com base no ID fornecido na rota.
-    //   // Suponha que você tenha uma função fetchProductById que retorna os detalhes do produto.
     //   fetchProductById(id)
     //     .then(data => setProduct(data))
     //     .catch(error => console.error('Error fetching product:', error));
@@ -40,37 +58,51 @@ export function Product() {
         <section className="productContainer">
             <hr />
             <div className='containerMain'>
-                <div className="containerImages">
-                    <img src='' alt="img1" className='mainImage'/>
-                    <div className="optionsImages">
-                        <img src='' alt="img1" />
-                        <img src='' alt="img2" />
-                        <img src='' alt="img3" />
-                        <img src='' alt="img4" />
+                <div className='productMain'>
+                    <div className="containerImages">
+                    <img src={mainImage} alt="Main" className='imageMain'/>
+                        <div className="optionsImages">
+                         {[
+                                "https://via.placeholder.com/150",
+                                "https://via.placeholder.com/150/0000FF/808080",
+                                "https://via.placeholder.com/150/FF0000/FFFFFF",
+                                "https://via.placeholder.com/150/00FF00/000000"
+                            ].map((image, index) => (
+                                <img
+                                    key={index}
+                                    src={image}
+                                    alt={`img${index}`}
+                                    onClick={() => handleImageClick(image)}
+                                />
+                            ))}
+                        </div>
                     </div>
-                </div>
-                <div className="containerInfos">
-                    {/* icon favorite Apple 27" iMac Desktop Computer (16GB RAM, 1TB HDD, Intel Core i5) */}
-                    <h3 className='productName'>{productData.name}</h3>
-                    <p className='priceOrigin'>{BRL.format(productData.price)}</p>
-                    <p className="productPrice">À vista <br />
-                        <span>{BRL.format(productData.price - (productData.price / 100 * productData.Discount))}</span>
-                    </p>
-                    <p className='productPricePix'><span>{BRL.format( (productData.price - (productData.price / 100 * productData.Discount) - (productData.price - (productData.price / 100 * productData.Discount)) / 100 * 15) )}</span>  <br />
-                        No PIX com 15% de desconto</p>
-                    <p className='priceFees'>
-                        Ou no Cartão <br />
-                        Em até 12x de {BRL.format((productData.price - (productData.price / 100 * productData.Discount)) / 12)} sem juros no cartão
-                    </p>
-                    <button className='addCart'>
-                        Adicionar ao Carrinho
-                        {/* icon */}
-                    </button>
+                    <div className="containerInfos">
+                        {isFavorite ? (
+                            <MdFavorite className='iconFav' onClick={handleAddToFavorites}/>
+                        ) : (
+                            <MdFavoriteBorder className='iconFav' onClick={handleAddToFavorites}/>
+                        )}
+                        <h3 className='productName'>{productData.name}</h3>
+                        <p className='priceOrigin'>{BRL.format(productData.price)}</p>
+                        <p className="productPrice">À vista <br />
+                            <span>{BRL.format(productData.price - (productData.price / 100 * productData.Discount))}</span>  <br />
+                            E no PIX com 15% de desconto
+                        </p>
+                        <p className='priceFees'>
+                            Ou no Cartão <br />
+                            Em até 12x de <span>{BRL.format((productData.price - (productData.price / 100 * productData.Discount)) / 12)}</span> sem juros
+                        </p>
+                        <button className='addCart' onClick={handleAddToCart}>
+                            Adicionar ao Carrinho
+                            <img src={iconAddCart} alt="icon add Cart" className='iconAdd' />
+                        </button>
+                    </div>
                 </div>
                 <div className='containerDetails'>
                     <div className='containerDescription'>
                         <h3>Descrição do Produto</h3>
-                        <p>{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}</p>
+                        <p>{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}{productData.description}</p>
                     </div>
                     <div className='containerSpecifications'>
                         <p className='specification'>
@@ -82,11 +114,11 @@ export function Product() {
                             <p>{productData.brand}</p>
                         </p>
                         <p className='specification'>
-                            <strong>Categoria</strong>
+                            <strong>Categoria:</strong>
                             <p>{productData.category}</p>
                         </p>
                         <p className='specification'>
-                            <strong>SubCategoria</strong>
+                            <strong>SubCategoria:</strong>
                             <p>{productData.subCategory}</p>
                         </p>
                         <p className='specification'>
